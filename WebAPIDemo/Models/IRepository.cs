ecc64a017ad343cba652df0e43949b126c529820
Michael Burns <michael.burns@civica.co.uk>
2016-07-21 16:25:07 +0100

    adding interface for Repository

---
+++ WebAPIDemo/Models/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+namespace WebAPIDemo.Models
+{
+    public interface IRepository
+    {
+        IQueryable<Order> GetAllOrders();
+        IQueryable<Order> GetAllOrdersWithDetails();
+        Order GetAllOrdersWithDetails(int Id);
+    }
+}
--- WebAPIDemo/Models/Repository.cs
+++ WebAPIDemo/Models/Repository.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 
 namespace WebAPIDemo.Models
 {
-    public class Repository
+    public class Repository : IRepository
     {
         #region Members
 
